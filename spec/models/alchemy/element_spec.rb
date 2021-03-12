ca6e2c0254c56ed9ff86c5e18b6d07dbf3992d48
Robin Böning <robin.boening@gmail.com>
2021-03-12 12:39:25 +0100

    Fixes failing test with Rails 6.1 on CI
    Even though it wasn't failing for me locally, it did fail on CI. This change hopefully fixes it.

--- spec/models/alchemy/element_spec.rb
+++ spec/models/alchemy/element_spec.rb
@@ -923,7 +923,7 @@ module Alchemy
         let!(:nested_element) { create(:alchemy_element, parent_element: element, page: page) }
 
         it "returns nested elements" do
-          expect(subject).to eq([nested_element])
+          expect(subject).to contain_exactly(nested_element)
         end
 
         context "with hidden nested elements" do
@@ -932,7 +932,7 @@ module Alchemy
           end
 
           it "does not include them" do
-            expect(subject).to eq([nested_element])
+            expect(subject).to contain_exactly(nested_element)
           end
         end
       end
